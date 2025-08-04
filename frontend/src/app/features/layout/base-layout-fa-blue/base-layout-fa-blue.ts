8b6714e1c41dfc4d4e7065da452bcad15f3683d0
cleme <clement.lemlijn@gmail.com>
2025-08-04 18:26:07 +0200

    adding folklore and base layout fa blue

---
+++ frontend/src/app/features/layout/base-layout-fa-blue/base-layout-fa-blue.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout-fa-blue',
+  imports: [],
+  templateUrl: './base-layout-fa-blue.html'
+})
+export class BaseLayoutFaBlue {
+
+}
