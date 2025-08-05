f9432b3515bc98e454fb841c24a50ee11d227e48
cleme <clement.lemlijn@gmail.com>
2025-08-05 13:33:52 +0200

    folklore subjects templates

--- frontend/src/app/features/folklore/tab-folklore/tab-folklore.ts
+++ frontend/src/app/features/folklore/tab-folklore/tab-folklore.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Card} from 'primeng/card';
 import {Fluid} from 'primeng/fluid';
+import {BaptismalSong, SongTypeEnum} from '../../../models/song.model';
+import {FolkloreSubject} from '../../../models/folklore-subject.model';
 
 @Component({
   selector: 'app-tab-folklore',
@@ -11,4 +13,27 @@ import {Fluid} from 'primeng/fluid';
 })
 export class TabFolklore {
 
+  protected folkloreSubjects: FolkloreSubject[] = [
+    { id: '1', title: 'L’esplanade des terrasses', content: '' },
+    { id: '2', title: 'L’exposition de 1905', content: '' },
+    { id: '4', title: 'Monuments Parc d’Avroy', content: '' },
+    { id: '5', title: 'Statue de charlemagne', content: '' },
+    { id: '6', title: 'L’Eglise Saint-Jacques-le-Mineur', content: '' },
+    { id: '7', title: 'Vertbois', content: '' },
+    { id: '8', title: 'Les moines de Saint-Bernardin', content: '' },
+    { id: '9', title: 'XX août', content: '' },
+    { id: '10', title: 'Liège et son Histoire', content: '' },
+    { id: '11', title: 'Tchantches', content: '' },
+    { id: '12', title: 'Dju d’la moûsse', content: '' },
+    { id: '13', title: 'Le Pont des Arches', content: '' },
+    { id: '14', title: 'Les Fontaines Montéfiores', content: '' },
+    { id: '15', title: 'Statue De G. Simenon', content: '' },
+    { id: '16', title: 'Le Perron', content: '' },
+    { id: '17', title: 'La Violette', content: '' },
+    { id: '18', title: 'Les marches de Bueren', content: '' },
+    { id: '19', title: 'La place Saint Lambert', content: '' },
+    { id: '20', title: 'Le palais des Princes Evêques', content: '' },
+    { id: '21', title: 'L’Opéra', content: '' },
+    { id: '22', title: 'La Vierge Delcourt', content: '' },
+  ];
 }
---
+++ frontend/src/app/models/folklore-subject.model.ts
@@ -0,0 +1,5 @@
+export interface FolkloreSubject {
+  id: string;
+  title: string;
+  content: string;
+}
