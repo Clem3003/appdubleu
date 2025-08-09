56f5cfdae994deb04a95dc56b696d18cd37bfd62
cleme <clement.lemlijn@gmail.com>
2025-08-09 16:37:25 +0200

    demo setup

--- frontend/src/app/features/features.routes.ts
+++ frontend/src/app/features/features.routes.ts
@@ -18,8 +18,8 @@ const defaultRoutes: Routes = [
 
 const childrenRoutes: Routes = [
     { path: 'dashboard', component: Dashboard },
-    { path: 'folklore-quiz', component: Folklore },
-    { path: 'folklore', component: FolkloreQuiz },
+    { path: 'folklore', component: Folklore },
+    { path: 'folklore-quiz', component: FolkloreQuiz },
     { path: 'forum', component: Forum },
     { path: 'profile', component: Profile },
   // {
